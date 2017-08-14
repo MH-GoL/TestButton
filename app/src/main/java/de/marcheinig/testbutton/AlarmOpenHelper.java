3042b607aef8bc6203491ed0a116881ca6eb2d49
MH-GoL <Marc_Heinig@gmx.de>
2017-08-14 16:22:09 +0200

    sql erweitert

--- app/src/main/java/de/marcheinig/testbutton/AlarmOpenHelper.java
+++ app/src/main/java/de/marcheinig/testbutton/AlarmOpenHelper.java
@@ -7,24 +7,25 @@ import android.database.sqlite.*;
 
 public class AlarmOpenHelper extends SQLiteOpenHelper
 {
-	private static final String _ID = "_id";
+	private static final String ID = "id";
 	private static final String TABLE_NAME = "alarmList";
 	private static final String ALARMLIST_TIME = "timeMillis";
-	private static final String ALARMLIST_NAME = "";
-	private static final String ALARMLIST_ALARMACTION_ID = "";
-	private static final String ALARMLIST_REPEAT = "";
+	private static final String ALARMLIST_NAME = "name";
+	private static final String ALARMLIST_ALARMACTION_ID = "actionId";
+	private static final String ALARMLIST_REPEAT = "repeat";
 	
-	private static final String CREATE_TABLE = "CREATE TABLE " +
-	TABLE_NAME + "("+ _ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ALARMLIST_TIME + " INTEGER, " + ALARMLIST_REPEAT + " INTEGER, " + ALARMLIST_NAME + " STRING, " + ALARMLIST_ALARMACTION_ID + " INTEGER);";
+	private static final String ALARMLIST_CREATE_TABLE = "CREATE TABLE " +
+	TABLE_NAME + "("+ ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + ALARMLIST_TIME + " INTEGER, " + ALARMLIST_REPEAT + " INTEGER, " + ALARMLIST_NAME + " STRING, " + ALARMLIST_ALARMACTION_ID + " INTEGER);";
+	private static final String ALARMLIST_DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
 	
 	@Override
-	public void onCreate(SQLiteDatabase p1)
+	public void onCreate(SQLiteDatabase db)
 	{
-		p1.execSQL(CREATE_TABLE);
+		db.execSQL(ALARMLIST_CREATE_TABLE);
 	}
 
 	@Override
-	public void onUpgrade(SQLiteDatabase p1, int p2, int p3)
+	public void onUpgrade(SQLiteDatabase db1, int db2, int db3)
 	{
 		// TODO: Implement this method
 	}
@@ -32,5 +33,16 @@ public class AlarmOpenHelper extends SQLiteOpenHelper
 	AlarmOpenHelper (Context context)
 	{
 		super(context, "", null, 1);
+	}
+	
+	public void deleteTable()
+	{
+		SQLiteDatabase db = getReadableDatabase();
+		db.execSQL(ALARMLIST_DROP_TABLE);
+	}
+	
+	public boolean insertDataAlarmlist(int timeMillis, int repeat, String name, int actionId)
+	{
+		return true;
 	}
 }
